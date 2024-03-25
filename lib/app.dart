7fda2fec74a870faa3f3b23fcfc7ff9498833a61
taufiqursabbir <taufiqur.sabbir11@gmail.com>
2024-03-25 13:34:10 +0600

    screen utils added and theme added

--- lib/app.dart
+++ lib/app.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:helth_io/view/UI/splash_screen.dart';
+import 'package:get/get.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class MyApp extends StatefulWidget {
   const MyApp({super.key});
@@ -10,6 +13,79 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return ScreenUtilInit(
+      designSize: Size(360, 640),
+    builder: (context, child)=> GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Task Manager',
+      theme: ThemeData(
+          brightness: Brightness.light,
+          primaryColor: Colors.blueAccent,
+          primarySwatch: Colors.blue,
+          inputDecorationTheme: const InputDecorationTheme(
+            contentPadding: EdgeInsets.symmetric(horizontal: 25, vertical: 16),
+            filled: true,
+            fillColor: Colors.white,
+            border: OutlineInputBorder(borderSide: BorderSide.none),
+          ),
+
+
+
+          textTheme: TextTheme(
+              titleLarge: TextStyle(
+                  fontWeight: FontWeight.w500,
+                  fontSize: 25,
+                  color: Colors.black,
+                  letterSpacing: 0.5
+              )
+          ),
+          elevatedButtonTheme: ElevatedButtonThemeData(
+              style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.blueAccent,
+                  padding: EdgeInsets.symmetric(vertical: 8),
+                  elevation: 3,
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(8)
+                  )
+              )
+          )
+      ),
+
+
+
+      darkTheme: ThemeData(brightness: Brightness.dark,
+          primaryColor: Colors.black,
+          primarySwatch: Colors.blue,
+          inputDecorationTheme: const InputDecorationTheme(
+            contentPadding: EdgeInsets.symmetric(horizontal: 25, vertical: 16),
+            filled: true,
+            fillColor: Colors.white,
+            hintStyle:  TextStyle(color: Colors.grey),
+            border: OutlineInputBorder(borderSide: BorderSide.none),
+          ),
+          textTheme: TextTheme(
+              titleLarge: TextStyle(
+                  fontWeight: FontWeight.w500,
+                  fontSize: 25,
+                  color: Colors.black,
+                  letterSpacing: 0.5
+              )
+          ),
+          elevatedButtonTheme: ElevatedButtonThemeData(
+              style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.blueAccent,
+                  padding: EdgeInsets.symmetric(vertical: 8),
+                  elevation: 3,
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(8)
+                  )
+              )
+          )
+      ),
+      themeMode: ThemeMode.light,
+      home: const Splash_screen(),
+    ));
+
+
   }
 }
