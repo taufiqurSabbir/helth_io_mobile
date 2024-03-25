dc6ef1f82ca9c26ddc20f1dd772e85a0d5e9a509
taufiqursabbir <taufiqur.sabbir11@gmail.com>
2024-03-25 13:34:59 +0600

    main dart file

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-
 import 'app.dart';
+import 'package:flutter/widgets.dart';
+
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
