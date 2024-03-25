fe0bdfc01f2ac9c458cf6f7175033625cb8880ad
taufiqursabbir <taufiqur.sabbir11@gmail.com>
2024-03-25 13:35:09 +0600

    phone login screen

---
+++ lib/view/UI/phone_login.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Phone_Login extends StatefulWidget {
+  const Phone_Login({super.key});
+
+  @override
+  State<Phone_Login> createState() => _Phone_LoginState();
+}
+
+class _Phone_LoginState extends State<Phone_Login> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
