71b511e5f96b4d3213976a5552d4c406751adcfa
taufiqurSabbir <taufiqur.sabbir11@gmail.com>
2024-04-06 12:10:24 +0600

    onboading asset added

--- lib/view/utils/asset_utils.dart
+++ lib/view/utils/asset_utils.dart
@@ -2,4 +2,7 @@ class AssetUtils{
   AssetUtils._();
   static const String _Imageloc ='asset/images';
   static String logoPng ='$_Imageloc/logo.png';
+  static String ob1 ='$_Imageloc/ob1.json';
+  static String ob2 ='$_Imageloc/ob2.json';
+  static String ob3 ='$_Imageloc/ob3.json';
 }
