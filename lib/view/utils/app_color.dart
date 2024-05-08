cd2910f63c5d530b90729ad443b13d36005d8c42
taufiqurSabbir <taufiqur.sabbir11@gmail.com>
2024-05-08 18:13:46 +0600

    app color added

---
+++ lib/view/utils/app_color.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const backgroundColor = Color(0xFFEAECEF);
+  static const primaryColor = Color(0xFF13A9F1);
+  static const secondaryColor = Color(0xFFB26D0E);
+  static const blue = Color(0xFF2F80ED);
+  static const darkBlue = Color(0xFF1B5BB0);
+  static const lightBlue = Color(0xFF529BFA);
+}
