aebea9a2362cc368e348a9d8ea1dcf6d2a4f3d17
taufiqurSabbir <taufiqur.sabbir11@gmail.com>
2024-05-08 18:15:19 +0600

    my booking page

---
+++ lib/view/UI/my_booking.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class MyBooking extends StatefulWidget {
+  const MyBooking({super.key});
+
+  @override
+  State<MyBooking> createState() => _MyBookingState();
+}
+
+class _MyBookingState extends State<MyBooking> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
