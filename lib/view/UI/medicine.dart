b796c2ed2c2f0ed5c5dcb80021133f086f5f6c5d
taufiqurSabbir <taufiqur.sabbir11@gmail.com>
2024-05-08 18:14:53 +0600

    Medicine Page added

---
+++ lib/view/UI/medicine.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class Medicine extends StatefulWidget {
+  const Medicine({super.key});
+
+  @override
+  State<Medicine> createState() => _MedicineState();
+}
+
+class _MedicineState extends State<Medicine> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
