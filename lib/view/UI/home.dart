95c978b899edfeef6029e4c60921a6acf54ecf5d
taufiqurSabbir <taufiqur.sabbir11@gmail.com>
2024-05-08 23:21:21 +0600

    app bar added and slider added

--- lib/view/UI/home.dart
+++ lib/view/UI/home.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import '../../widgets/HomeSlider.dart';
+import '../utils/asset_utils.dart';
 
 
 class Home extends StatefulWidget {
@@ -11,6 +14,52 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
-  }
-}
+    return Scaffold(
+      appBar:  AppBar(
+        backgroundColor: Colors.white,
+        centerTitle: false,
+        automaticallyImplyLeading: false,
+        title: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            SizedBox(height: 1.h,),
+            Image.asset(AssetUtils.logoPng,height: 100.h,width: 130.w,),
+            SizedBox(height: 1.h,),
+          ],
+        ),
+        actions: [
+          IconButton(
+            onPressed: (){
+
+            },
+            icon: const Icon(Icons.search),
+          ),
+        ],
+      ),
+
+      body: SafeArea(
+        child: ListView(
+          padding: const EdgeInsets.only(top: 10, bottom: 25),
+          children: [
+        Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+
+
+            SizedBox(
+              height: 1.h,
+            ),
+            HomeSlider(),
+            SizedBox(
+              height: 3.h,
+            ),
+          ],
+        ),
+      ),
+      ],
+    ),
+    ),
+    );
+}}
